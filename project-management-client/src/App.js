fa78ff1a8413e5478bbfee1ef79d8586c6dc7fb0
osmanbozoglu <iosmanbozoglu@gmail.com>
2021-03-10 23:19:05 +0300

    Client-side implementation.

--- project-management-client/src/App.js
+++ project-management-client/src/App.js
@@ -1,15 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome Project Management App</h1>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/addProject" component={AddProject} />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
