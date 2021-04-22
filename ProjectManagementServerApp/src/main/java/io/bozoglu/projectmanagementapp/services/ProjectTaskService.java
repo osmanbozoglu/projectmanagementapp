24a17efb86ceef7cee01beea1d8d8d470c4e2111
osmanbozoglu <iosmanbozoglu@gmail.com>
2021-04-22 20:35:29 +0300

    Finding project task by project sequence is implemented.

--- ProjectManagementServerApp/src/main/java/io/bozoglu/projectmanagementapp/services/ProjectTaskService.java
+++ ProjectManagementServerApp/src/main/java/io/bozoglu/projectmanagementapp/services/ProjectTaskService.java
@@ -52,4 +52,21 @@ public class ProjectTaskService {
             );
         return projectTasks;
     }
+
+    public ProjectTask findByProjectSequence(String backlog_id, String pt_id){
+        Backlog backlog = backlogRepository.findByProjectIdentifier(backlog_id);
+        if(backlog == null){
+            throw new ProjectNotFoundException("Project with ID: " + backlog_id + " does not exist");
+        }
+
+        ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
+        if(projectTask == null){
+            throw new ProjectNotFoundException("Project Task '" + pt_id+"' not found");
+        }
+
+        if(!projectTask.getProjectIdentifier().equals(backlog_id)){
+            throw new ProjectNotFoundException("Project Task '" + pt_id + "' does not exist in project: '" + backlog_id + "'");
+        }
+        return projectTask;
+    }
 }
